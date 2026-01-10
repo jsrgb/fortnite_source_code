adffa523a3b6c3f2837ed4676c3264906a0aa557
Ja Morphy <maps@jax2.net>
2026-01-10 14:50:05 -0800

    remove build shell script

--- build_shaders.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/zsh
-xcrun -sdk macosx metal -c src/shaders/transform.metal -o src/shaders/transform.air && xcrun -sdk macosx metallib src/shaders/transform.air -o src/shaders/transform.metallib
